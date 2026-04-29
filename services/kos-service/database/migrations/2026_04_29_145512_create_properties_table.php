97fe747bc68f6932e52e9053304963ebecc36163
Fakhry <fakhryzackyputra@gmail.com>
2026-04-29 22:20:59 +0700

    feat: inisialisasi laravel 11 dan implementasi skema database relasional 4 tabel untuk layanan kos

---
+++ services/kos-service/database/migrations/2026_04_29_145512_create_properties_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignId('owner_id')->constrained('owners')->onDelete('cascade');
+            $table->string('nama_kos');
+            $table->text('alamat');
+            $table->text('deskripsi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
